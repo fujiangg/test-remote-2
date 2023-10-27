d2dce4585f1e37d3c055c3293263f29f5bc4f819
fuji <fuji@gmail.com>
2023-10-27 09:49:58 +0700

    update file index.php dengan menambahkan paragraf baru

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo labore unde autem maiores consectetur obcaecati laborum. Qui repellendus consequuntur et velit dolor voluptatibus sed aperiam? Consequatur minima ipsam facilis aliquid.</p>
 </body>
 </html>
